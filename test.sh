6f650bf3474cb6e76e55a68d09deca54bec171b3
Richard Yao <richardyao1985@gmail.com>
2014-10-29 17:06:32 +0800

    initial version

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIRNAME=`dirname $0`
+REALPATH=`which realpath`
+if [ ! -z "${REALPATH}" ]; then
+  DIRNAME=`realpath ${DIRNAME}`
+fi
+
+if [ "$HPHP_HOME" != "" ]; then
+    HHVM="${HPHP_HOME}/hphp/hhvm/hhvm"
+else
+    HHVM=hhvm
+fi
+
+$HHVM \
+  -vDynamicExtensions.0=${DIRNAME}/mongo.so \
+  ${DIRNAME}/test.php
